907f8b57e9b2f9601e40c6f5627f45c39a275dab
oaao <oliver.antoniu@gmail.com>
2020-07-30 17:40:09 -0600

    [prj] fix /target/ inclusion

--- rust-intro/target/debug/deps/rust_intro-56b6333ba249c5ce.d
+++
@@ -1,5 +0,0 @@
-/home/oaao/_workspace/rust-intro/rust-intro/target/debug/deps/rust_intro-56b6333ba249c5ce: src/main.rs
-
-/home/oaao/_workspace/rust-intro/rust-intro/target/debug/deps/rust_intro-56b6333ba249c5ce.d: src/main.rs
-
-src/main.rs:
--- rust-intro/target/debug/deps/rust_intro-5deffb58c81ae4d6.d
+++
@@ -1,5 +0,0 @@
-/home/oaao/_workspace/rust-intro/rust-intro/target/debug/deps/rust_intro-5deffb58c81ae4d6.rmeta: src/main.rs
-
-/home/oaao/_workspace/rust-intro/rust-intro/target/debug/deps/rust_intro-5deffb58c81ae4d6.d: src/main.rs
-
-src/main.rs:
