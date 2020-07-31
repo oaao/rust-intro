398549ec2e59bdcf8bcfa571b0a1c44f1fe80720
oaao <oliver.antoniu@gmail.com>
2020-07-30 18:11:54 -0600

    [rev] guessing-game - rng implementation

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,15 +1,24 @@
 use std::io;
 // from std import io, in familiar neanderthal
+use rand::Rng;
+// Rng is a trait and defines methods that need to be in scope for us to use them
+// paired with their usage below, i don't fully understand this yet. 
+// is it similar to implicit importing?
 
 fn main() {
 
-	println!("guess the number!");
+	println!("try to guess the secret number! (1-100)");
 	println!("please input your guess.");
 
 	let mut guess = String::new();
 		// let x = 1; // creates immutable var
 		// let mut x = 1; // creates mutable var
 		// Type::staticmethod(); :: signals an associated function of a type, rather than a .method() of its instance
+
+	let secret_num = rand::thread_rng().gen_range(1, 101);
+		// thread_rng() is local to current exec thread, and seeded by OS
+		// gen_range is a method defined by Rng trait 
+		// note that gen_range is inclusive on lower bound and exclusive on upper bound
 
 	io::stdin() 
 		// same thing here - stdin() method of the io module
@@ -26,5 +35,6 @@ fn main() {
 
 	println!("your guess was: {}", guess);
 	// fairly familiar string interpolation
+	println!("the secret number was: {}", secret_num);
 
 }
